1128e52138011f6141bdea8aae6c598393fa9e37
piotrhudaszek <hudaszekpiotr@gmail.com>
2022-01-21 12:05:46 +0100

    sds

---
+++ test/test_app.py
@@ -0,0 +1,34 @@
+from app import bubble_sort
+import pytest
+
+
+
+
+
+def test1_bubble_sort():
+    arr = [1,2,3,4,5]
+    expected_output = [1,2,3,4,5]
+    assert bubble_sort(arr) == expected_output
+ 
+def test2_bubble_sort():
+    arr = [5,4,3,2,1]
+    expected_output = [1,2,3,4,5]
+    assert bubble_sort(arr) == expected_output
+
+def test3_bubble_sort():
+    arr = [0,12,1,4,5,2,1]
+    expected_output = [0,1,1,2,4,5,12]
+    assert bubble_sort(arr) == expected_output
+
+def test4_descending_bubble_sort():
+    arr = [0,12,1,4,5,2,1]
+    expected_output = [12,5,4,2,1,1,0]
+    assert bubble_sort(arr, False) == expected_output
+
+def test5_large_list_sort():
+    arr = []
+    expected_output = []
+    assert bubble_sort(arr, False) == expected_output
+
+
+
