15bdb8811c09f2de55984ac0683d09788c6b6929
hudaszekpiotr <hudaszekpiotr@gmail.com>
2022-01-21 21:29:37 +0100

    asfgf

--- test/test_app.py
+++ test/test_app.py
@@ -1,11 +1,15 @@
 from app import bubble_sort
 import pytest
+import random
 
 
 
+def test_empty_bubble_sort():
+    arr = []
+    expected_output = []
+    assert bubble_sort(arr) == expected_output
 
-
-def test1_bubble_sort():
+def test_bubble_sort():
     arr = [1,2,3,4,5]
     expected_output = [1,2,3,4,5]
     assert bubble_sort(arr) == expected_output
@@ -20,15 +24,17 @@ def test3_bubble_sort():
     expected_output = [0,1,1,2,4,5,12]
     assert bubble_sort(arr) == expected_output
 
+def test5_large_list_sort():
+    arr = list(random.sample(range(-20, 100000), 5000))
+    expected_output = sorted(arr)
+    assert bubble_sort(arr) == expected_output
+
 def test4_descending_bubble_sort():
     arr = [0,12,1,4,5,2,1]
     expected_output = [12,5,4,2,1,1,0]
     assert bubble_sort(arr, False) == expected_output
 
-def test5_large_list_sort():
-    arr = []
-    expected_output = []
-    assert bubble_sort(arr, False) == expected_output
+
 
 
 
